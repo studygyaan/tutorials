6dd8e7c58e949a602c609c976dcce2ae8388c75a
professionalcipher <professionalcipher@gmail.com>
2023-07-26 01:53:50 +0530

    postgres conf

---
+++ spring-boot-postgresql-configuration/src/main/java/com/example/myproject/User.java
@@ -0,0 +1,40 @@
+package com.example.myproject;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    // Constructors, Getters, and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
---
+++ spring-boot-postgresql-configuration/src/main/java/com/example/myproject/UserService.java
@@ -0,0 +1,52 @@
+package com.example.myproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    // Create a new user
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // Get all users
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // Get user by ID
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    // Update user
+    public User updateUser(Long id, User userDetails) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            User existingUser = user.get();
+            existingUser.setName(userDetails.getName());
+            existingUser.setEmail(userDetails.getEmail());
+            return userRepository.save(existingUser);
+        }
+        return null;
+    }
+
+    // Delete all users
+    public void deleteAllUsers() {
+        userRepository.deleteAll();
+    }
+
+    // Delete user
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    // Other business logic related to users
+}
