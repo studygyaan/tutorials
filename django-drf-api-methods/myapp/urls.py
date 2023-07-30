32cfac9f558852bb4b83154741391e7d9172e953
professionalcipher <professionalcipher@gmail.com>
2023-07-31 04:38:22 +0530

    django api methods

--- django-drf-api-methods/myapp/urls.py
+++ django-drf-api-methods/myapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import HelloWorldAPIView, BookListView, BookDetailView, BookCreateView
+from .views import HelloWorldAPIView, BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView, BookView
 
 urlpatterns = [
     path('hello/', HelloWorldAPIView.as_view(), name='hello_world'),
     path('api/books/', BookListView.as_view(), name='book-list'),
     path('api/books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('api/books/', BookCreateView.as_view(), name='book-create'),
+    path('api/books/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('api/books/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
+    path('api/books/', BookView.as_view(), name='book-list'),
+    path('api/book/<int:pk>/', BookView.as_view(), name='book-detail'),
 ]
+
--- django-drf-api-methods/myapp/views.py
+++ django-drf-api-methods/myapp/views.py
@@ -4,6 +4,10 @@ from rest_framework.response import Response
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 class HelloWorldAPIView(APIView):
     def get(self, request):
@@ -40,3 +44,53 @@ class BookDetailView(generics.RetrieveAPIView):
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+# PUT Method
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# DELETE Method
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Combining Methods in the View
+class BookView(APIView):
+    def get(self, request, pk=None):  # Add 'pk=None' to the get method
+        if pk is not None:
+            book = Book.objects.get(pk=pk)
+            serializer = BookSerializer(book)
+            return Response(serializer.data)
+        else:
+            books = Book.objects.all()
+            serializer = BookSerializer(books, many=True)
+            return Response(serializer.data)
+
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):  # Add 'pk=None' to the put method
+        if pk is not None:
+            book = Book.objects.get(pk=pk)
+            serializer = BookSerializer(book, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'Please provide a valid book ID.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk=None):  # Add 'pk=None' to the delete method
+        if pk is not None:
+            book = Book.objects.get(pk=pk)
+            book.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            books = Book.objects.all()
+            books.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
