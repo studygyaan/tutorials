e37036c01074ae9fdb3baff6ad53ba26de3bc8f9
professionalcipher <professionalcipher@gmail.com>
2023-08-02 07:03:43 +0530

    changes

--- django-drf-authentication/accounts/signals.py
+++ django-drf-authentication/accounts/signals.py
@@ -29,8 +29,8 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
     }
 
     # render email text
-    email_html_message = render_to_string('email/user_reset_password.html', context)
-    email_plaintext_message = render_to_string('email/user_reset_password.txt', context)
+    email_html_message = render_to_string('email/password_reset_email.html', context)
+    email_plaintext_message = render_to_string('email/password_reset_email.txt', context)
 
     msg = EmailMultiAlternatives(
         # title:
