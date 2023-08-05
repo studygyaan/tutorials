0cc629a68e688ee8036d053e64ec8d5346fa155c
professionalcipher <professionalcipher@gmail.com>
2023-08-05 06:01:24 +0530

    django ml model

--- DataScience/Linear Regression - House Price Predictions/load_pkl_file_to_predict.py
+++ DataScience/Linear Regression - House Price Predictions/load_pkl_file_to_predict.py
@@ -1,20 +1,16 @@
 import pickle
 
 # Load the model from the .pkl file
-with open('linear_regression_model.pkl', 'rb') as file:
+with open('/kaggle/input/picklefile/linear_regression_model.pkl', 'rb') as file:
     loaded_model = pickle.load(file)
     
 import pandas as pd
 
-# Load the test dataset from CSV or create a DataFrame with the same format
-test_data = pd.read_csv('data.csv')  # Replace 'test_data.csv' with the path to your test dataset
-
-# Extract the features from the test data
-new_data = [[3, 2, 1500, 4000, 1, 0, 0, 3]]
+# Create a DataFrame with the new data in the same format as the training data
+new_data = pd.DataFrame([[3, 2, 1500, 4000, 1, 0, 0, 3]], columns=['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors', 'waterfront', 'view', 'condition'])
 
 # Make predictions using the loaded model
-predictions = loaded_model.predict(new_data)
+predicted_price = loaded_model.predict(new_data)
 
-# If you want to see the predictions, you can convert the predictions array to a DataFrame
-predictions_df = pd.DataFrame(predictions, columns=['predicted_price'])
-print(predictions_df)
+print("Predicted Price:", predicted_price[0])
+
