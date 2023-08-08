2acae44837112fd56b6c6061e01a12a6206f91e5
professionalcipher <professionalcipher@gmail.com>
2023-08-09 02:45:11 +0530

    thymeleaf spring boot

---
+++ spring-boot-thymeleaf/README.md
@@ -0,0 +1,1 @@
+Tutorial: https://studygyaan.com/spring-boot/thymeleaf-and-bootstrap-5-template-engine-in-spring-boot
---
+++ spring-boot-thymeleaf/src/main/java/com/example/myproject/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.myproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("greeting", "Welcome to our dynamic website!");
+        return "index";
+    }
+}
