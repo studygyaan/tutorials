447f21f3583b9bf6095adf756cc8273dab4b3dca
professionalcipher <professionalcipher@gmail.com>
2023-08-21 23:59:02 +0530

    django crispy form

--- django-bootstrap5/myapp/views.py
+++ django-bootstrap5/myapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import MyForm
 
-def hello_world(request):
-    return HttpResponse("Hello, world!")
+def hello_view(request):
+    if request.method == 'POST':
+        form = MyForm(request.POST)
+        if form.is_valid():
+            # Do something with the form data
+            pass
+    else:
+        form = MyForm()
+
+    return render(request, 'home.html', {'form': form})
--- django-bootstrap5/myproject/settings.py
+++ django-bootstrap5/myproject/settings.py
@@ -38,7 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
+    'crispy_forms',
+    'crispy_bootstrap5', 
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
