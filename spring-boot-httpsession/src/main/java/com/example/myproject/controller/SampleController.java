3cbd23275c514df02df69d8f40ada26f16363862
professionalcipher <professionalcipher@gmail.com>
2023-08-24 00:15:11 +0530

    changes

--- spring-boot-httpsession/src/main/java/com/example/myproject/controller/SampleController.java
+++ spring-boot-httpsession/src/main/java/com/example/myproject/controller/SampleController.java
@@ -1,4 +1,6 @@
 package com.example.myproject.controller;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,4 +13,27 @@ public class SampleController {
     public String sayHello() {
         return "Hello, Spring Boot!";
     }
+
+    @GetMapping("/storeData")
+    public String storeData(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        // Store data in the session
+        session.setAttribute("username", "john_doe");
+        return "data_stored";
+    }
+
+    @GetMapping("/getData")
+    public String getData(HttpSession session) {
+        String username = (String) session.getAttribute("username");
+        return "Hello, " + username;
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        // Invalidate the session
+        session.invalidate();
+        return "logged_out";
+    }
+
+
 }
