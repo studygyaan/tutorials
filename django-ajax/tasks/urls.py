6a3e601b0c701311ac51b4358146093e84d4611b
professionalcipher <professionalcipher@gmail.com>
2023-08-26 05:35:46 +0530

    django ajax crud update delete

--- django-ajax/tasks/urls.py
+++ django-ajax/tasks/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.tasks, name='tasks'),
     path('get_tasks/', views.get_tasks, name='get_tasks'),
     path('add_task/', views.add_task, name='add_task'),
+    path('update_task_status/', views.update_task_status, name='update_task_status'),
+    path('delete_task/', views.delete_task, name='delete_task'),
 ]
--- django-ajax/tasks/views.py
+++ django-ajax/tasks/views.py
@@ -9,11 +9,31 @@ def tasks(request):
 
 def get_tasks(request):
     tasks = Task.objects.all()
-    task_list = [{'title': task.title} for task in tasks]
+    task_list = [{'id': task.id, 'title': task.title, 'completed': task.completed} for task in tasks]
     return JsonResponse(task_list, safe=False)
 
 def add_task(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         task = Task.objects.create(title=title)
+        return JsonResponse({'status': 'success'})
+
+def update_task_status(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        completed = request.POST.get('completed')
+        if completed == 'true':
+            completed = True
+        else:
+            completed = False
+        task = Task.objects.get(id=id)
+        task.completed = completed
+        task.save()  # Save the changes to the task
+        return JsonResponse({'status': 'success'})
+
+def delete_task(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        task = Task.objects.get(id=id)
+        task.delete()
         return JsonResponse({'status': 'success'})
