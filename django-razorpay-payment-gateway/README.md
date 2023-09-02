a05a818ba39db8f6d5e6303df8668b385f57d297
Huzaif Sayyed <huzaif.work@gmail.com>
2023-09-02 18:08:12 +0530

    django razorpay

---
+++ django-razorpay-payment-gateway/README.md
@@ -0,0 +1,1 @@
+Find this Tutorial on https://studygyaan.com/django/django-razorpay-payment-gateway-integration-tutorial
---
+++ django-razorpay-payment-gateway/payment_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PaymentAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'payment_app'
