5fca8d793b35756f91a1515fa59fc1543489eabc
Huzaif Sayyed <huzaif.work@gmail.com>
2023-09-07 04:47:10 +0530

    django admin

---
+++ django/django-models/myapp/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
