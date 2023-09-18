afd1ff80413c0f91e7d1ac9eda12ea62e3dc11ae
studygyaan <studygyaan@gmail.com>
2023-09-18 13:08:02 +0530

    chnages

--- django/django-user-register-login-logout/myproject/settings.py
+++ django/django-user-register-login-logout/myproject/settings.py
@@ -44,8 +44,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'studygyaan@gmail.com'
-EMAIL_HOST_PASSWORD = 'rnmspwnwndzidzpp'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_gmail_app_password'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
